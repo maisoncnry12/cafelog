b122c7f5a98d5addc2eb81986e0554663e2ea263
maisoncnry12 <caxxry@gmail.com>
2021-11-08 09:10:17 +0000

    [Add]ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+
   devise_for :users
   get '/' => 'homes#top'
+  get 'home/about' => 'homes#about'
+
+  resources :users, only: [:show, :edit, :update]do
+    # フォロー/フォロワー
+  resource :relationships,only: [:create, :destroy]
+  end
+
+  resources :posts,except: [:show]do
+    resources :favorites, only: [:index, :create, :destroy]
+    resource :comments, only: [:create, :destroy]
+  end
+
+  get "/search"=>"searchs#search"
+
 end
