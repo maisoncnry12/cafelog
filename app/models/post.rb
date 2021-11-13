00ce2baf5ceec3f1649da6a41e0ab76b50872880
maisoncnry12 <caxxry@gmail.com>
2021-11-13 07:45:24 +0000

    [Add]いいね機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  # has_many :favorites, dependent:destroy
+  
+  has_many :favorites
+   def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+   end
+   
   has_one :area
   
   attachment :post_image
