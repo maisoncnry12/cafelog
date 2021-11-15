bd561e3b6058910b5951570460ec6a30996d52e9
maisoncnry12 <caxxry@gmail.com>
2021-11-15 02:59:31 +0000

    [Fix]コメント機能投稿/削除の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,22 @@
 class CommentsController < ApplicationController
-  
+
   def create
     @post = Post.find(params[:post_id])
-    @comment = current_user.comments.new(comment_params)
-    @comment.post_id = post.id
+    @comment = Comment.new(comment_params)
+    @comment.post_id = @post.id
+    @comment.user_id = current_user.id
     @comment.save
-    redirect_to posts_path
+     redirect_to post_path(@post.id)
   end
-  
+
   def destroy
-    Comment.find_by(id: params[:id]).destroy
-    redirect_to posts_path(params[:post_id])
+    Comment.find_by(id: params[:id],post_id: params[:post_id]).destroy
+     redirect_to request.referer
   end
-  
+
   private
   def comment_params
     params.require(:comment).permit(:user_id, :post_id, :comment)
   end
+
 end
