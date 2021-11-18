6f4b914fb8aa2caf7b0f2339b7c6f91381d1317d
maisoncnry12 <caxxry@gmail.com>
2021-11-18 01:54:31 +0000

    [Add]検索機能

---
+++ app/controllers/searchs_controller.rb
@@ -0,0 +1,9 @@
+class SearchsController < ApplicationController
+  before_action :authenticate_user!
+
+	def search
+    @posts = Post.search(params[:keyword])
+    @keyword = params[:keyword]
+  end
+  
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,19 @@
 class Post < ApplicationRecord
+  
+  attachment :post_image
   belongs_to :user
   has_many :comments, dependent: :destroy
-  #Post.commentsで投稿が所有するコメント取得できる
-  
+  has_one :area
+ 
+  # いいね機能
   has_many :favorites
    def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
    end
    
-  has_one :area
-  
-  attachment :post_image
+   # 検索機能
+   def self.search(keyword)
+    @posts = Post.where(["shop_name like? OR body like?", "%#{keyword}%", "%#{keyword}%"])
+   end
   
 end
