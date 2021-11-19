a21128effd409fccf4231ce5717975ae243ca9de
maisoncnry12 <caxxry@gmail.com>
2021-11-19 06:39:02 +0000

    [Add]top画像スライダー

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
          has_many :posts, dependent: :destroy
          has_many :comments, dependent: :destroy
-         #User.commentsで、ユーザーの所有するコメントを取得できる
          has_many :favorites, dependent: :destroy
          # フォロー/フォロワーのアソシエーション
          has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
